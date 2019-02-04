419a9992db5e18b31f0b18d6e5f0035e2dd8919b
Terry Jones <tjones@arataki.school.nz>
2019-02-04 19:02:05 +1300

    Create constant.h

---
+++ constant.h
@@ -0,0 +1,7 @@
+// Header for program
+// (c) Terry Jones 2019 
+// tedzjones@protonmail.ch
+#define servicelinux 50.00
+#define servicewindows 60.00
+#define calloutfee 30.00
+#define remoteassist 50.00
